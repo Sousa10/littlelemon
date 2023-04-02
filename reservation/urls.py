92e2e9ce2dfef7d6a681483e2792a4dc32508c81
Rui Matias Silva de Sousa <cvt00947@cvt.cv>
2023-04-02 21:54:37 -0100

    added menu and booking api

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,7 +1,11 @@
 #define URL route for index() view
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('', views.index, name='index'),
 ]
